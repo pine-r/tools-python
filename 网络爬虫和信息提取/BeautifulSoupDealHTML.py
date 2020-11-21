830d8c5ccf16e8a11c506d8174e031eacd90653d
rentingsong <1296670091@qq.com>
2020-11-21 20:32:53 +0800

    beautifulsoup4遍历

--- 网络爬虫和信息提取/BeautifulSoupDealHTML.py
+++ 网络爬虫和信息提取/BeautifulSoupDealHTML.py
@@ -19,8 +19,33 @@ if __name__ == "__main__":
     demo = get_html(url)
     soup = BeautifulSoup(demo, "html.parser")    # 解释器html.parse
     print(soup.title)  # 打印html页面的title信息
+    # 标签树的下行遍历
+    print("head子节点信息：", soup.head.contents)
+    print("body 子节点信息：", soup.body.contents)
+    print("body 子节点信息的第二个元素：", soup.body.contents[1])
+    for child in soup.body.children:
+        print("遍历儿子节点", child)
+    for child in soup.body.descendants:
+        print("遍历子孙节点：", child)
 
-    # 获取标签
+    # 标签树的上行遍历
+    print("head 标签父节点", soup.head.parent)
+    for parent in soup.head.parents:
+        if parent is None:
+            print(parent)
+        else:
+            print("head 上行遍历：", parent.name)
+
+    # 标签树的平行遍历
+    print("a标签的后一个平行标签: ", soup.a.next_sibling)
+    print("a标签的前一个平行标签: ", soup.a.previous_sibling)
+    for sibling in soup.a.next_siblings:
+        print("遍历后续节点：", sibling)
+
+    for sibling in soup.a.previous_siblings:
+        print("遍历前续节点：", sibling)
+
+   # 获取标签
     tag = soup.a  # 打印出所有的a标签中第一个
     print("第一个a标签中的内容：", tag)
     a_name = soup.a.name  # a标签的名字
